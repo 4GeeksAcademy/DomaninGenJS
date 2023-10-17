5a901bc7f47a2c6c852bf3e004faf23c3943c296
juanguille42-prog <77317575+juanguille42-prog@users.noreply.github.com>
2023-10-17 00:09:08 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,21 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  let pronoun = ["the", "our", "their", "her"];
-  let adj = ["great", "big", "wonderful", "magic"];
-  let noun = ["jogger", "racoon", "cat", "dog"];
-  let dot = [".com", ".net", ".us", ".io"];
+  // Define arrays
+  const pronouns = ["the", "our", "their", "her"];
+  const adjectives = ["great", "big", "wonderful", "magic"];
+  const nouns = ["jogger", "racoon", "cat", "dog"];
+  const extensions = [".com", ".net", ".us", ".io"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    let iPronoun = pronoun[i];
-    for (let j = 0; j < adj.length; j++) {
-      let jAdj = adj[j];
-      for (let k = 0; k < noun.length; k++) {
-        let kNoun = noun[k];
-        for (let l = 0; l < dot.length; l++) {
-          let lDot = dot[l];
-          console.log(iPronoun + jAdj + kNoun + lDot);
+  // Generate domain names
+  for (const pronoun of pronouns) {
+    for (const adjective of adjectives) {
+      for (const noun of nouns) {
+        for (const extension of extensions) {
+          console.log(`${pronoun}${adjective}${noun}${extension}`);
         }
       }
     }
